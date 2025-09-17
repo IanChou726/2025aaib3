4279237026a6de9953178de7f8ecd5185989be27
IanChou726 <ianchou950726@gmail.com>
2025-09-17 11:33:21 +0800

    week02

---
+++ week02-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+    cout<<"½Ð¿é¤J¼Æ¦r";
+    string a, ans;
+    cin >> a;
+    for(int i=a.length()-1;i>=0;i--)
+    {
+        ans+= a[i];
+
+    }
+    cout << a << ans;
+}
