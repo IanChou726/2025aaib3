9e04ce132614bb982e2eae993c8f532e081d90ce
IanChou726 <ianchou950726@gmail.com>
2025-11-05 11:48:51 +0800

    week09

---
+++ week09-3a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+int main(){
+	string a;
+	cin >> a;
+	for (int i = 0;i<a.length();i++){
+		if(isupper(a[i]))a[i] = tolower(a[i]);
+		else if (islower(a[i]))a[i] = toupper(a[i]);
+	}
+	cout << a <<endl;
+}
