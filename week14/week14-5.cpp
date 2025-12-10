7c80d0db5e48bf487cce221cb8518c3d72065f94
IanChou726 <ianchou950726@gmail.com>
2025-12-10 12:03:12 +0800

    week14

---
+++ week14/week14-5.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    while (cin >> N){
+        if(N<0) break;
+        int ans = 1+(1+N)*N/2;
+        cout<<ans<<"\n";
+    }
+}
